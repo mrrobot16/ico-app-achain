d50a1d5df428ae6c8a923700f98ab1d2edead0c0
Jedi <hmgaar@gmail.com>
2017-10-21 13:02:24 -0400

    Created TermAgreement Model

---
+++ db/migrate/20171021170031_create_term_agreements.rb
@@ -0,0 +1,11 @@
+class CreateTermAgreements < ActiveRecord::Migration[5.1]
+  def change
+    create_table :term_agreements do |t|
+      t.string :shareholder_address
+      t.string :term_accepted
+      t.text :terms_description
+
+      t.timestamps
+    end
+  end
+end
