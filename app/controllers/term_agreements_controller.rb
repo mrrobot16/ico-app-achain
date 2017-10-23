47cdb1b79a0e31fb2db664b824f704991cf82c22
Jedi <hmgaar@gmail.com>
2017-10-23 00:25:05 -0400

    added new field in TermAgreement to count amount of people with same_address requesting a contract address, added checkbox and validation to check user checked all boxes, added contract page where user will copy paste our contract address

--- app/controllers/term_agreements_controller.rb
+++ app/controllers/term_agreements_controller.rb
@@ -1,15 +1,46 @@
 class TermAgreementsController < ApplicationController
+  def index
+    @term_agreements = TermAgreement.all
+  end
 
   def new
     @term_agreement = TermAgreement.new
   end
+
   def create
+    shareholder_address = term_agreement_params[:shareholder_address]
+    @term_agreement = TermAgreement.find_by_shareholder_address(shareholder_address)
+    if @term_agreement
+      cookies.encrypted[:accepted_terms] = true
+      @term_agreement.counter = @term_agreement.counter + 1
+      redirect_to '/contract'
+    else
+      @term_agreement = TermAgreement.new(shareholder_address: shareholder_address)
+      @term_agreement.term_accepted = true if accepted_all_terms?
+      respond_to do |format|
+        if @term_agreement.save
+          cookies.encrypted[:accepted_terms] = true
+          format.html { redirect_to contract_path }
+        else
+          format.html { render :new }
+        end
+      end
+    end
 
   end
 
   private
-  def term_agreement_params
-    params.require(:term_agreement).permit(:shareholder_address, :term_accepted)
-  end
+    def accepted_all_terms?
+      has_check_one = (term_agreement_params[:check_one].to_i === 1)
+      has_check_two = (term_agreement_params[:check_two].to_i === 1)
+      has_check_three = (term_agreement_params[:check_three].to_i === 1)
+      has_check_one && has_check_two && has_check_three
+    end
 
+    def term_agreement_params
+      params.require(:term_agreement).permit(
+        :shareholder_address,
+        :check_one, :check_two, :check_three
+      )
+    end
 end
--- db/migrate/20171021170031_create_term_agreements.rb
+++ db/migrate/20171021170031_create_term_agreements.rb
@@ -3,8 +3,7 @@ class CreateTermAgreements < ActiveRecord::Migration[5.1]
     create_table :term_agreements do |t|
       t.string :shareholder_address
       t.string :term_accepted
-      t.text :terms_description
-
+      t.integer :counter, default: 0
       t.timestamps
     end
   end
