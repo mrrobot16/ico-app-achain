ec66e262017f2047f42ffad67302861e3828a0a1
Jedi <hmgaar@gmail.com>
2017-12-15 22:01:34 -0500

    refactored css for pages controller

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,9 +6,11 @@ var counter = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById('days').innerHTML = days+' :'
-  document.getElementById('hours').innerHTML = hours+' :'
-  document.getElementById('minutes').innerHTML = minutes+' :'
-  document.getElementById('seconds').innerHTML = seconds
+  if(document.getElementById('days')){
+    document.getElementById('days').innerHTML = days+' :'
+    document.getElementById('hours').innerHTML = hours+' :'
+    document.getElementById('minutes').innerHTML = minutes+' :'
+    document.getElementById('seconds').innerHTML = seconds
+  }
   distance < 0 ? clearInterval(counter) : ''
 }, 1000);
